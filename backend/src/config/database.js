c64de7958e8980f512c36030b5181b9d3b12cf30
guuhx97 <gustavorobertotb@outlook.com>
2020-02-01 09:59:07 -0300

    :construction_worker: Feat: Added authentication by token using JWT

---
+++ backend/src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  database: 'gympoint',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
